c04519c13998daf57a61da9d1d086f9efb383e03
Fearchar MacLean <46941430+Fearchar@users.noreply.github.com>
2021-06-24 15:22:36 +0100

    Changed conclusion to sucess

--- .github/actions/simple-check/index.js
+++ .github/actions/simple-check/index.js
@@ -17,7 +17,7 @@ async function run() {
       name: "Simple Check",
       head_sha: gitHubSha,
       status: "completed",
-      conclusion: "failure",
+      conclusion: "success",
       /* todo: fm - do I need output? */
       output: {
         title: "Check Created by API",
