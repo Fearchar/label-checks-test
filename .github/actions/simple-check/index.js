e682c31fff72c082b0c945e33c0c0a3710df0428
Fearchar MacLean <46941430+Fearchar@users.noreply.github.com>
2021-07-14 08:49:22 +0100

    index.js refactor

--- .github/actions/simple-check/index.js
+++ .github/actions/simple-check/index.js
@@ -1,41 +1,66 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+
+function getActionDetails() {
+  const gitHubToken = core.getInput("github-token");
+  const pullNumber = core.getInput("pull-number");
+
+  const [gitHubRepoOwner, gitHubRepoName] =
+    process.env.GITHUB_REPOSITORY.split("/");
+
+  return { gitHubToken, pullNumber, gitHubRepoOwner, gitHubRepoName };
+}
+
+function getPullRequest(
+  gitHubToken,
+  gitHubRepoOwner,
+  gitHubRepoName,
+  pullNumber
+) {
+  const octokit = github.getOctokit(gitHubToken);
+
+  const response = await octokit.rest.pulls.get({
+    owner: gitHubRepoOwner,
+    repo: gitHubRepoName,
+    pull_number: pullNumber,
+  });
+
+  if (!response.status === 200) {
+    core.setFailed(`There is no pull request with the number ${pullNumber}`);
+  }
+
+  return response.data;
+}
+
 /* todo: fm - rename run*/
-async function run() {
+async function runAction() {
   /* todo: fm - get ride of all the uneeded mentions of github */
   try {
-    const [gitHubRepoOwner, gitHubRepoName] =
-      process.env.GITHUB_REPOSITORY.split("/");
-    const gitHubSha = process.env.GITHUB_SHA;
-    const gitHubToken = core.getInput("github-token");
-    const pullNumber = core.getInput("pull-number");
+    const { gitHubToken, gitHubRepoOwner, gitHubRepoName, pullNumber } =
+      getActionDetails();
 
-    const octokit = github.getOctokit(gitHubToken);
+    const pullRequest = getPullRequest(
+      gitHubToken,
+      gitHubRepoOwner,
+      gitHubRepoName,
+      pullNumber
+    );
 
-    const pullResponse = await octokit.rest.pulls.get({
-      owner: gitHubRepoOwner,
-      repo: gitHubRepoName,
-      pull_number: pullNumber,
-    });
-    const pull = pullResponse.data;
-
-    if (!pullResponse.status === 200) {
-      core.setFailed(`There is no pull request with the number ${pullNumber}`);
-    }
-
-    if (!pull.labels.find((label) => label.name === "QA Passed")) {
+    if (!pullRequest.labels.find((label) => label.name === "QA Passed")) {
+      /* note: fm - returns aren't strictly needed. they're for the benifit of code clarity*/
       core.setFailed(
         'Pull requests require the "QA Passed" label before they can be merged.'
       );
+      return;
     }
 
     core.setOutput(
       "labels",
-      JSON.stringify(pull.labels.map((label) => label.name))
+      JSON.stringify(pullRequest.labels.map((label) => label.name))
     );
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-run();
+runAction();
