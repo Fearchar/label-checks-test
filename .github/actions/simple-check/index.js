8a516611a8398c1e47d955b992292aadd6588e62
Fearchar MacLean <46941430+Fearchar@users.noreply.github.com>
2021-07-14 09:06:46 +0100

    removed the word "github" where it wasn't adding to code clarity

--- .github/actions/simple-check/index.js
+++ .github/actions/simple-check/index.js
@@ -2,26 +2,20 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 function getActionDetails() {
-  const gitHubToken = core.getInput("github-token");
+  const token = core.getInput("github-token");
   const pullNumber = core.getInput("pull-number");
 
-  const [gitHubRepoOwner, gitHubRepoName] =
-    process.env.GITHUB_REPOSITORY.split("/");
+  const [repoOwner, repoName] = process.env.GITHUB_REPOSITORY.split("/");
 
-  return { gitHubToken, pullNumber, gitHubRepoOwner, gitHubRepoName };
+  return { token, pullNumber, repoOwner, repoName };
 }
 
-function getPullRequest(
-  gitHubToken,
-  gitHubRepoOwner,
-  gitHubRepoName,
-  pullNumber
-) {
-  const octokit = github.getOctokit(gitHubToken);
+function getPullRequest(token, repoOwner, repoName, pullNumber) {
+  const octokit = github.getOctokit(token);
 
   const response = await octokit.rest.pulls.get({
-    owner: gitHubRepoOwner,
-    repo: gitHubRepoName,
+    owner: repoOwner,
+    repo: repoName,
     pull_number: pullNumber,
   });
 
@@ -32,22 +26,13 @@ function getPullRequest(
   return response.data;
 }
 
-/* todo: fm - rename run*/
 async function runAction() {
-  /* todo: fm - get ride of all the uneeded mentions of github */
   try {
-    const { gitHubToken, gitHubRepoOwner, gitHubRepoName, pullNumber } =
-      getActionDetails();
+    const { token, repoOwner, repoName, pullNumber } = getActionDetails();
 
-    const pullRequest = getPullRequest(
-      gitHubToken,
-      gitHubRepoOwner,
-      gitHubRepoName,
-      pullNumber
-    );
+    const pullRequest = getPullRequest(token, repoOwner, repoName, pullNumber);
 
     if (!pullRequest.labels.find((label) => label.name === "QA Passed")) {
-      /* note: fm - returns aren't strictly needed. they're for the benifit of code clarity*/
       core.setFailed(
         'Pull requests require the "QA Passed" label before they can be merged.'
       );
